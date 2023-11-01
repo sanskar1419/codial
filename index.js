cfb39f84a638ff7151c4e1c8a6523779b8750063
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2023-11-01 11:39:29 +0530

    First commit : Setup our directory and fire the express server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+const port = 9000;
+
+// We will make the application listen to server
+app.listen(port, function (err) {
+  // In printing we have used back tick and interpolation in order to print
+  if (err) {
+    console.log(`Error in running the Server : ${err}`);
+  }
+  console.log(`Server is running in port : ${port}`);
+});
