104518aefc7b0c7d3e549c3c472b515fe94f49f1
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2023-11-01 18:01:45 +0530

    Setup the home_controller.js in Controller

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,10 @@
 const express = require("express");
 const router = express.Router();
-// console.log("File is connected");
+
+// Aquiring the Home_controller file module
+const homeController = require("../controllers/home_controller");
+console.log("File is connected");
+
+// We are using the home function here
+router.get("/", homeController.home);
 module.exports = router;
