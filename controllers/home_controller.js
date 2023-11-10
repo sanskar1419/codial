7941e5a75fc68ea7bd2765b0f8d0adf713fd960d
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2023-11-10 14:19:57 +0530

    Pre populate user and display comments and the user post that comment

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,11 +1,19 @@
-module.exports.home = function (req, res) {
+const Post = require("../models/post");
+module.exports.home = async function (req, res) {
   // To print and manipulate the key value pair data present in the cookie
   // console.log(req.cookies);
   // res.cookie("user_id", 35);
+  try {
+    const posts = await Post.find({}).populate("user");
+    return res.render("home", {
+      title: "Codial Home",
+      posts: posts,
+    });
+  } catch (err) {
+    console.log("Unable to find post ", err);
+    return;
+  }
 
-  return res.render("home", {
-    title: "Codial Home",
-  });
   // return res.end("<h1>Express is up for Codial</h1>");
 };
 
