546afb24a53b180c03d5f591ba19abb3ce6cea96
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2023-12-06 09:57:32 +0530

    Define user schema for profile photo uploading

--- models/user.js
+++ models/user.js
@@ -1,4 +1,10 @@
 const mongoose = require("mongoose");
+
+// Using multer for the file upload
+const multer = require("multer");
+const path = require("path");
+const AVATAR_PATH = path.join("/uploads/users/avatars");
+
 const userSchema = new mongoose.Schema(
   {
     email: {
@@ -14,11 +20,27 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    avatar: {
+      type: String,
+    },
   },
   {
     timestamps: true,
   }
 );
+
+let storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, path.join(__dirname, "../", AVATAR_PATH));
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(null, file.fieldname + "-" + uniqueSuffix);
+  },
+});
+
+// Static Function
+userSchema.statics.uploadAvatar = multer({ storage: storage });
 
 const User = mongoose.model("User", userSchema);
 module.exports = User;
