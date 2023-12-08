572125b9e31b7585aa6e1eda303ff0a3b575a905
SANSKAR GUPTA <sanskarguptabaisali@gmail.com>
2023-12-08 15:41:59 +0530

    Setup passport JWT

---
+++ config/passport-jwt-strategies.js
@@ -0,0 +1,29 @@
+const passport = require("passport");
+const JWTStrategy = require("passport-jwt").Strategy;
+const ExtractJwt = require("passport-jwt").ExtractJwt;
+
+const User = require("../models/user");
+
+let opts = {
+  //Header have a list and out of this list we have a key authentication and it also have a list key value pair and one of them is Bearer
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken,
+  secretOrKey: "Codial",
+};
+
+passport.use(
+  new JWTStrategy(opts, async function (jwtPayload, done) {
+    try {
+      const user = await User.findById(jwtPayload._id);
+      if (user) {
+        return done(null, user);
+      } else {
+        return done(null, false);
+      }
+    } catch (err) {
+      console.log("Error in finding out the user ", err);
+      return;
+    }
+  })
+);
+
+module.exports = passport;
--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const db = require("./config/mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocal = require("./config/passport-local-strategies");
+const passportJWT = require("./config/passport-jwt-strategies");
 const MongoStore = require("connect-mongo");
 const sassMiddleware = require("node-sass-middleware");
 // used for flash messeging
